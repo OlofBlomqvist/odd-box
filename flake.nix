1998ab23e29cdb71f5adeadc717103a8ff2082fc
Olof Blomqvist <olof@twnet.se>
2024-09-15 23:23:33 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         packages.default = pkgs.rustPlatform.buildRustPackage rec {
 
             pname = "odd-box";
-            version = "0.1.2";
+            version = "0.1.3";
             src = ./.;
 
             cargoLock = {
