330a429a32497a8526e8fd799f52c0b72f4e7aaf
Olof <olof@twnet.se>
2025-03-27 21:07:48 +0100

    up flake

--- flake.nix
+++ flake.nix
@@ -23,8 +23,8 @@
 
         packages.default = pkgs.rustPlatform.buildRustPackage rec {
 
-            pname = "odd-box";
-            # version = "0.1.10";
+            name = "odd-box";
+            version = "0.1.11";
             src = ./.;
 
             cargoLock = {
