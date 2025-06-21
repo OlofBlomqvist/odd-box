83b7d9a79706da8c11f667e2bd075c64f241299f
Olof <olof@twnet.se>
2025-06-21 16:10:15 +0000

    add basic login page to webui

--- web-ui/src/hooks/use-hosted-sites.ts
+++ web-ui/src/hooks/use-hosted-sites.ts
@@ -1,6 +1,7 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Api } from "../generated-api";
 import checkIsHostedProcess from "../lib/is_hosted_process";
+import { deleteCookie, getCookie } from "@/utils/cookies"; 
 
 export const useHostedSites = () => {
   let hostName = window.location.protocol + "//" + window.location.hostname
@@ -13,11 +14,26 @@ export const useHostedSites = () => {
       ? `${import.meta.env.VITE_ODDBOX_API_URL}:${import.meta.env.VITE_ODDBOX_API_PORT}`
       : hostName;
   
-  const apiClient = new Api({ baseUrl });
+  
+  const apiClient = new Api({ baseUrl});
 
   return useSuspenseQuery({
     queryKey: ["sites"],
-    queryFn: apiClient.api.list,
+    queryFn: async () => { 
+      try {
+          const password = getCookie("password");  
+          var result = await apiClient.api.list({
+            headers: {
+              Authorization: `${password || ""}`
+            } 
+          }); 
+          return result;
+        } catch(e:any) {
+          if(e.status === 403) {
+            deleteCookie("password");
+          }
+          throw e;
+        }},
     select: (res) =>
       res.data.items.filter(checkIsHostedProcess).map((x) => x.HostedProcess),
   });
