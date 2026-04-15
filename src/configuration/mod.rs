58decb58c13b144146571f3afae0f5c2692bb8a0
Olof Blomqvist <olof@twnet.se>
2026-04-15 23:37:26 +0200

    binwatch

--- src/configuration/mod.rs
+++ src/configuration/mod.rs
@@ -271,6 +271,7 @@ pub fn v3_to_cruma(v3: &v3::V3Config) -> Result<TunnelCliConfiguration, String> 
                 upstream_tls: proc.https.unwrap_or(false),
                 backend_timeout_seconds: Some(60), // legacy behavior was unlimited while new default using None is 10 sec. lets do 1min at least
                 idle_timeout_seconds: None,
+                binary_watch: None,
             });
 
             // Frontend routing
