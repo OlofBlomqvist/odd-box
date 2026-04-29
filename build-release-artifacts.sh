71b49961ee93bc08428bac1414a0179e07619def
zdx <olof@twnet.se>
2026-04-29 13:58:06 +0200

    fix build script

--- build-release-artifacts.sh
+++ build-release-artifacts.sh
@@ -68,7 +68,7 @@ ALL_TARGETS=(
 )
 
 DEFAULT_TARGETS=(
-  "x86_64-unknown-linux-gnu",
+  "x86_64-unknown-linux-gnu"
   "x86_64-pc-windows-msvc"
   "x86_64-apple-darwin-dmg"
   "aarch64-apple-darwin"
